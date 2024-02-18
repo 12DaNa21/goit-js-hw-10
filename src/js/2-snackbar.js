507187c90c4be8585d341b05bc14e1ced7b0a80f
12DaNa21 <145206474+12DaNa21@users.noreply.github.com>
2024-02-18 19:43:24 +0200

    HW10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,37 @@
-console.log("SNACKBAR");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const delay = Number(document.querySelector('input[name="delay"]').value);
+  const status = document.querySelector('input[name="state"]:checked').value;
+
+  const promise = new Promise((resolve, reject) => {
+    if (status === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else if (status === 'rejected') {
+      reject(delay);
+    }
+  });
+
+  promise.then((value) => {
+    iziToast.show({
+      
+      message: `✅ Fulfilled promise in ${delay}ms`
+  });
+
+
+
+    
+  }).catch((value) => {
+    iziToast.show({
+      message: `❌ Rejected promise in ${delay}ms`
+    });
+  });
+  
+});
